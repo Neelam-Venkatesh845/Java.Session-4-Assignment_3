415a3a64c9699d6a2e9b37dbb5e7cb10f5f26113
Neelam-Venaktesh <n.venkateshvvv@gmail.com>
2016-09-14 19:53:22 +0530

    Assignment 3 of Session 4 GitHub commit
    Assignment 3 of Session 4 GitHub commit

---
+++ Assignment3.java
@@ -0,0 +1,13 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setName("Neelam");
+		System.out.println("name is "+emp.getName());	
+		
+		emp.setEmpId(28664);
+		System.out.println("Employee id is " +emp.getEmpId());
+	}
+
+}
